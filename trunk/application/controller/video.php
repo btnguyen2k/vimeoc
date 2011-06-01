107b9b7c85243819fa2a845abc84212f204541c7
tringuyen@ongsoft.com <tringuyen@ongsoft.com@ef7c459b-88f4-fe9e-c7e7-bc0618af2582>
2011-06-01 03:50:38 +0000

    apply assign function for controller

--- trunk/application/controller/video.php
+++ trunk/application/controller/video.php
@@ -36,16 +36,16 @@
 			{
 				$this->loadModel('model_user');
 				$user = $this->model_user->getUserByUserId($userId);
-				$this->tmpl->assign('userAvatar', $user['avatar']);
+				$this->assign('userAvatar', $user['avatar']);
 			}
 			
-			$this->tmpl->assign("videobasicinfo", $this->loadMessages('user.video.link.basicinfo'));
-			$this->tmpl->assign("videothumbnail", $this->loadMessages('user.video.link.thumbnail'));
-			$this->tmpl->assign("videovideofile", $this->loadMessages('user.video.link.videofile'));
-			$this->tmpl->assign("videocustomurl", $this->loadMessages('user.video.link.customurl'));
-			$this->tmpl->assign("videoaddto", $this->loadMessages('user.video.link.addto'));
-			$this->tmpl->assign("videodelete", $this->loadMessages('user.video.link.delete'));
-			$this->tmpl->assign("videobacktovideo", $this->loadMessages('user.video.link.backtovideo'));
+			$this->assign("videobasicinfo", $this->loadMessages('user.video.link.basicinfo'));
+			$this->assign("videothumbnail", $this->loadMessages('user.video.link.thumbnail'));
+			$this->assign("videovideofile", $this->loadMessages('user.video.link.videofile'));
+			$this->assign("videocustomurl", $this->loadMessages('user.video.link.customurl'));
+			$this->assign("videoaddto", $this->loadMessages('user.video.link.addto'));
+			$this->assign("videodelete", $this->loadMessages('user.video.link.delete'));
+			$this->assign("videobacktovideo", $this->loadMessages('user.video.link.backtovideo'));
 		}
 		/**
 		 * Load messages source for videosetting page
@@ -56,14 +56,14 @@
 		{
 			$this->defaultVideoMessagesSource();
 			
-			$this->tmpl->assign("name", $this->loadMessages('user.videosetting.name'));
-			$this->tmpl->assign("title", $this->loadMessages('user.videosetting.title'));
-			$this->tmpl->assign('description', $this->loadMessages('user.videosetting.description'));
-			$this->tmpl->assign('tag', $this->loadMessages('user.videosetting.tags'));
+			$this->assign("name", $this->loadMessages('user.videosetting.name'));
+			$this->assign("title", $this->loadMessages('user.videosetting.title'));
+			$this->assign('description', $this->loadMessages('user.videosetting.description'));
+			$this->assign('tag', $this->loadMessages('user.videosetting.tags'));
 
-			$this->tmpl->assign('titleiInvalid', $this->loadErrorMessage('error.video.title'));
-			$this->tmpl->assign('descriptionInvalid', $this->loadErrorMessage('error.video.description'));
-			$this->tmpl->assign('tagInvalid', $this->loadErrorMessage('error.video.tag'));
+			$this->assign('titleiInvalid', $this->loadErrorMessage('error.video.title'));
+			$this->assign('descriptionInvalid', $this->loadErrorMessage('error.video.description'));
+			$this->assign('tagInvalid', $this->loadErrorMessage('error.video.tag'));
 		}
 		/**
 		 *view and action for videosetting form 
@@ -79,10 +79,10 @@
 				$tagid=$_GET['tagid'];
 				$tcid=$_GET['tcid'];
 				$video= $this->model_video->getvideofromvideoId(array($videoid));
-				$this->tmpl->assign('title_', $video['video_title']);
-				$this->tmpl->assign('description_', $video['description']);
+				$this->assign('title_', $video['video_title']);
+				$this->assign('description_', $video['description']);
 				$tags=$this->model_video->gettagfromtagandtagcomponent(array($tagid,$tcid));
-				$this->tmpl->assign('tag_', $tags);
+				$this->assign('tag_', $tags);
 				$this->loadTemplate('view_video_videosetting');
 				// Session to get videoid, tagid and tcid
 			}
