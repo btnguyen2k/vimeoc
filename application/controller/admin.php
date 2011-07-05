a858946ae1d3b69c027d62900443e055db6567c2
haipham <haipham@ongsoft.com>
2011-07-05 07:09:24 +0000

    0000197

--- application/controller/admin.php
+++ application/controller/admin.php
@@ -20,19 +20,227 @@
 		function index(){
 			$this->redirect($this->ctx().'/auth/login');
 		}
-		
+		/**
+		 * load message source for user list
+		 * 
+		 */
+		function userListMessagesSource()
+		{
+			$this->assign("title",$this->loadMessages('admin.userlist.title'));
+		}
+			
 		function userList()
 		{
-			if(!$this->isAdminLogged()){
-				$this->redirect($this->ctx().'/auth/login');
-			}	
+//			if(!$this->isAdminLogged()){
+//				$this->redirect($this->ctx().'/auth/login');
+//			}	
+			$_search_obj = unserialize($_SESSION['USER_SEARCH']);
+			
+			$_sort_modes = array(
+				1 => 'Newest',
+				2 => 'Oldest',
+				3 => 'Alphabetical'
+			);
+			$_sort_columns = array(
+				1 => 'creation_date',
+				2 => 'creation_date',
+				3 => 'album_name'
+			);
+			$_sort_orders = array(
+				1 => 'DESC',
+				2 => 'ASC',
+				3 => 'ASC'
+			);
+			$_page_sizes = array(
+				1 => 'All',//all videos
+				2 => 2,
+				3 => 3,
+				4 => 50
+			);
+			$_default_sort_mode = 1;
+			$_default_page_size = 2;
+			$_default_search_term = '';
+			
+			$users = array();
+			$pagination = '';
+			//$_reset = $_GET['reset'];// reset all value for display
+			if($_reset){
+				
+			}else{
+				if($_GET['sort']){
+					$_sort_mode = $_GET['sort'];
+					if(!in_array($_sort_mode, array_keys($_sort_modes), false)){
+						$_sort_mode = $_default_sort_mode;
+					}
+				}else{
+					$_sort_mode = $_search_obj->sort ? $_search_obj->sort : $_default_sort_mode;
+				}
+				if($_GET['psize']){
+					$_page_size = $_GET['psize'];
+					if(!in_array($_page_size, array_keys($_page_sizes), false)){
+						$_page_size = $_default_page_size;
+					}
+				}else{
+					$_page_size = $_search_obj->psize ? $_search_obj->psize : $_default_page_size;
+				}
+				if(in_array('term', array_keys($_GET))){
+					$_search_term = trim($_GET['term']);
+				}else{
+					$_search_term = $_search_obj->term ? $_search_obj->term : $_default_search_term;
+				}
+			}
+			$page = $_GET['page'] ? $_GET['page'] : '1';			
+			if(!ctype_digit($page)){
+				$this->redirect($this->ctx().'/user/album/');
+			}else{
+				$page = intval($page);
+			}
+			
+			$limit = is_int($_page_sizes[$_page_size]) ? $_page_sizes[$_page_size] : 0;
+			$offset = ($page - 1) * $limit;
+			$sort_column = $_sort_columns[$_sort_mode];
+			$sort_order = $_sort_orders[$_sort_mode];
+			
+			$_search_obj->sort = $_sort_mode;
+			$_search_obj->psize = $_page_size;
+			$_search_obj->term = $_search_term;
+			$_SESSION['ALBUM_SEARCH'] = serialize($_search_obj);
+			
+			$this->loadModel('model_album');
+			$model_album = $this->model_album;
+			$album_count = $model_album->countAlbumByUserId($userId, $limit, $offset, $_search_term, $sort_column, $sort_order);
+			if($album_count > 0){
+				if($limit > 0){
+					if($limit && ($page > ceil($album_count / $limit))){
+						$this->redirect($this->ctx().'/user/album/');
+					}
+					$albums = $model_album->selectAlbumsByUserId($userId, $limit, $offset, $_search_term, $sort_column, $sort_order);
+
+					$adjacents = 2;
+					$targetpage = $_SERVER['REDIRECT_URL'];
+					if(!($targetpage[strlen($targetpage) - 1] == '/')){
+						$targetpage .= '/';
+					}
+					if ($page == 0){
+						$page = 1;					//if no page var is given, default to 1.
+					}
+					$prev = $page - 1;							//previous page is page - 1
+					$next = $page + 1;							//next page is page + 1
+					$lastpage = ceil($album_count / $limit);		//lastpage is = total pages / items per page, rounded up.
+					$lpm1 = $lastpage - 1;						//last page minus 1
+					
+					/* 
+						Now we apply our rules and draw the pagination object. 
+					*/
+					$pagination = "";
+					if($lastpage > 1)
+					{
+						$pagination .= "<div class=\"pagination\">";
+						//previous button
+						if ($page > 1) 
+							$pagination.= "<a href=\"$targetpage?page=$prev\">« Previous</a>";
+						else
+							$pagination.= "<span class=\"disabled\">« Previous</span>";	
+						
+						//pages	
+						if ($lastpage < 7 + ($adjacents * 2))	//not enough pages to bother breaking it up
+						{	
+							for ($counter = 1; $counter <= $lastpage; $counter++)
+							{
+								if ($counter == $page)
+									$pagination.= "<span class=\"current\">$counter</span>";
+								else
+									$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+							}
+						}
+						elseif($lastpage > 5 + ($adjacents * 2))	//enough pages to hide some
+						{
+							//close to beginning; only hide later pages
+							if($page < 1 + ($adjacents * 2))		
+							{
+								for ($counter = 1; $counter < 4 + ($adjacents * 2); $counter++)
+								{
+									if ($counter == $page)
+										$pagination.= "<span class=\"current\">$counter</span>";
+									else
+										$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+								}
+								$pagination.= "...";
+								$pagination.= "<a href=\"$targetpage?page=$lpm1\">$lpm1</a>";
+								$pagination.= "<a href=\"$targetpage?page=$lastpage\">$lastpage</a>";		
+							}
+							//in middle; hide some front and some back
+							elseif($lastpage - ($adjacents * 2) > $page && $page > ($adjacents * 2))
+							{
+								$pagination.= "<a href=\"$targetpage?page=1\">1</a>";
+								$pagination.= "<a href=\"$targetpage?page=2\">2</a>";
+								$pagination.= "...";
+								for ($counter = $page - $adjacents; $counter <= $page + $adjacents; $counter++)
+								{
+									if ($counter == $page)
+										$pagination.= "<span class=\"current\">$counter</span>";
+									else
+										$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+								}
+								$pagination.= "...";
+								$pagination.= "<a href=\"$targetpage?page=$lpm1\">$lpm1</a>";
+								$pagination.= "<a href=\"$targetpage?page=$lastpage\">$lastpage</a>";		
+							}
+							//close to end; only hide early pages
+							else
+							{
+								$pagination.= "<a href=\"$targetpage?page=1\">1</a>";
+								$pagination.= "<a href=\"$targetpage?page=2\">2</a>";
+								$pagination.= "...";
+								for ($counter = $lastpage - (2 + ($adjacents * 2)); $counter <= $lastpage; $counter++)
+								{
+									if ($counter == $page)
+										$pagination.= "<span class=\"current\">$counter</span>";
+									else
+										$pagination.= "<a href=\"$targetpage?page=$counter\">$counter</a>";					
+								}
+							}
+						}
+						
+						//next button
+						if ($page < $counter - 1){
+							$pagination.= "<a href=\"$targetpage?page=$next\">Next »</a>";
+						}else{
+							$pagination.= "<span class=\"disabled\">Next »</span>";
+						}
+						$pagination.= "</div>\n";		
+					}
+				}else{
+					$albums = $model_album->selectAlbumsByUserId($userId, $limit, $offset, $_search_term, $sort_column, $sort_order);
+				}
+			}else{
+				$this->assign('message', 'No album');
+			}
+			
+			if(is_array($albums) && (count($albums) > 0)){
+				foreach($albums as &$album){
+					$album['thumbnail'] = empty($album['thumbnails_path']) ? $this->ctx() . '/images/icon-album.gif' : ($this->ctx() . $this->loadResources('image.upload.path') . $album['thumbnails_path']);
+				}
+			}
+			
+			
+			$this->assign('albums', $albums);
+			$this->assign('pagination', $pagination);
+			$this->assign('sort_modes', $_sort_modes);
+			$this->assign('page_sizes', $_page_sizes);
+			
+			$this->assign('sort_mode', $_sort_mode);
+			$this->assign('page_size', $_page_size);
+			$this->assign('search_term', $_search_term);
+			$this->assign('page', $page);
+			
+			$this->loadTemplate(USER_TEMPLATE_DIR.'view_user_album');
 		}
 		
 		function disableAccount()
 		{
 //			if(!$this->isAdminLogged()){
 //				$this->redirect($this->ctx().'/auth/login');
-//				return;
 //			}	
 			$this->loadModel('model_user');
 			if ($_SERVER['REQUEST_METHOD'] == 'GET')
@@ -140,17 +348,16 @@
 				}
 				else 
 				{
-					$password2=$_POST['password'];
 					$params = array($fullName, $username, $this->encodePassword($password), $username);
 					$userId = $this->model_user->addNewUser($params);
 					$userAlias = 'user'.$userId;
-					$getrole=$this->model_user->getRole(array());
+					$roles=$this->model_user->getRole(array());
 					$this->model_user->updateUserAlias(array($userAlias, $userId));					
 					$params = array($userId);
 					$user = $this->model_user->getUserByUserId($params);
-					$user['password']=$password2;
+					$user['password']=$password;
 					$this->model_user->updateUserRole(array($role,$userId));
-					$this->assign("getrole",$getrole);
+					$this->assign("getrole",$roles);
 					$this->sendingEmailWithSmarty('mail_welcome', 'user', $user, null, $user['email']);
 					$this->assign("success",$this->loadMessages('auth.thankyou.success'));
 					$this->assign("login",$this->loadMessages('auth.thankyou.login'));
