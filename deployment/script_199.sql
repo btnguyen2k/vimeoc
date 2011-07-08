e267d5bb03843cfa2f33917eba5b571216713129
haipham <haipham@ongsoft.com>
2011-07-08 02:17:02 +0000

    update script 0000199

--- deployment/script_199.sql
+++ deployment/script_199.sql
@@ -2,3 +2,7 @@ CREATE TABLE configuration (
          name VARCHAR(255),
          value INT(1)
        );
+       
+       
+INSERT INTO configuration(name) VALUES ('SHOW_LOGIN_FORM')
+INSERT INTO configuration(name) VALUES ('SHOW_SIGNUP_FORM')
