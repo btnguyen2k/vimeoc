291156e984d6763fc92539ec7ca0aaac299ab994
haipham@ongsoft.com <haipham@ongsoft.com@ef7c459b-88f4-fe9e-c7e7-bc0618af2582>
2011-07-18 09:36:13 +0000

    0000226

--- trunk/application/controller/content.php
+++ trunk/application/controller/content.php
@@ -32,12 +32,13 @@
 		 * function action load
 		 */
 		function load(){
-			$this->loadModel('model_content');
+			
 			// load content via content alias
 			$alias = $_GET['alias'];
 			// load content via content id
 			$id = $_GET['id'];
 			// check if alias exist, then load by alias ortherwise, try the content id
+			$this->loadModel('model_content');
 			$res= $this->model_content->isAliasExists(array($alias));
 			$resId = $this->model_content->isIdExists(array($id));
 			if($res==0)
