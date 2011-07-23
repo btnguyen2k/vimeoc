708a5060deae7484b26b00f207982c37805afa81
haipham@ongsoft.com <haipham@ongsoft.com@ef7c459b-88f4-fe9e-c7e7-bc0618af2582>
2011-07-23 04:45:42 +0000

    script category

--- trunk/deployment/script_category.sql
+++ trunk/deployment/script_category.sql
@@ -1,11 +1,11 @@
 CREATE TABLE category(
-	id bigint( 20 ),
+	id bigint( 20 ) NOT NULL AUTO_INCREMENT,
 	name varchar(255),
 	PRIMARY KEY (`id`)
 );
 ALTER TABLE `content` ADD `category_id` bigint(20) NULL;
-INSERT INTO category(name) VALUES ('system');
-INSERT INTO category(name) VALUES ('user');
+INSERT INTO category(id,name) VALUES (1,'system');
+INSERT INTO category(id,name) VALUES (2,'user');
 
 ALTER TABLE content
 ADD FOREIGN KEY (category_id)
