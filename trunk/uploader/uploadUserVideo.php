c2dba2e6e39d821974031e004061d1aec9030b06
tringuyen@ongsoft.com <tringuyen@ongsoft.com@ef7c459b-88f4-fe9e-c7e7-bc0618af2582>
2011-08-02 06:16:52 +0000

    update cancel file upload

--- trunk/uploader/uploadUserVideo.php
+++ trunk/uploader/uploadUserVideo.php
@@ -13,7 +13,8 @@ $model_user = $uploader->getModel('model_user');
 if($uploader->getLoggedUser() > 0){		
 	$user = $model_user->getUserByUserId(array($uploader->getLoggedUser()));
 	$ret = $uploader->upload($uploader->loadResources('video.upload.path'), $uploader->loadResources('video.upload.thumbnails'), $uploader->loadResources('video.upload.ext.support'), $uploader->loadResources('video.upload.maxsize'));
-	if(!is_numeric($ret)){
+	$fileExists = $uploader->isFileExists($uploader->loadResources('video.upload.path'), $ret);
+	if(!is_numeric($ret) && $fileExists){
 		echo $ret;
 	}else{
 		echo 'invalid-file.error';
